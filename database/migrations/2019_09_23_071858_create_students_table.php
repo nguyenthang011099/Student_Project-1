ec29a19dec58295cd7454da1f9892481639fb899
Nguyen Thang <nguyenthang011099.hust@gmail.com>
2019-09-23 15:21:26 +0700

    student table

---
+++ database/migrations/2019_09_23_071858_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->string('Student ID');
+            $table->text('FirstName');
+            $table->text('LastName');
+            $table->text('Address');
+            $table->text('Telephone');
+            $table->text('Email');
+            $table->text('PassWord');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
